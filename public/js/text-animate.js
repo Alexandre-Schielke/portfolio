7d127be25be550a62795cca80e88216f606b23bd
Alexandre Schielke <alexandre@codezeus.com.br>
2024-03-03 17:36:49 -0300

    teste

--- public/js/text-animate.js
+++ public/js/text-animate.js
@@ -2,6 +2,7 @@ const el = document.querySelector("#text");
 const text = '"A única maneira de fazer um ótimo trabalho é amar o que você faz"\n- Steve Jobs';
 const  interval = 40;
 
+
 function showtext(el, text, interval) {
     const char = text.split("").reverse();
 
